80f9b958a3916329c5c8b02e27a78acb102f70e6
GonzaPiccinini <gonzipiccinini@gmail.com>
2023-01-27 21:05:31 -0300

    Fin del patron

--- src/02-component-patterns/components/ProductButtons.tsx
+++ src/02-component-patterns/components/ProductButtons.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useCallback, useContext } from 'react'
 import { ProductContext } from './ProductCard'
 import styles from '../styles/styles.module.css'
 
@@ -8,16 +8,27 @@ export interface Props {
 }
 
 export const ProductButtons = ({ className, style }: Props) => {
-    const { increaseBy, counter } = useContext(ProductContext)
-  
-    return (
-        <div 
-          className={`${styles.buttonsContainer} ${className}`}
-          style={style}
-        >
-          <button onClick={() => increaseBy(-1)} className={styles.buttonMinus}>-</button>
-          <div className={styles.countLabel}>{counter}</div>
-          <button onClick={() => increaseBy(+1)} className={styles.buttonAdd}>+</button>
-        </div>
-    )
+    
+  const { increaseBy, counter, maxCount } = useContext(ProductContext)
+
+  const isMaxReached = useCallback(() => !!maxCount && counter === maxCount, [counter, maxCount])
+
+  return (
+      <div 
+        className={`${styles.buttonsContainer} ${className}`}
+        style={style}
+      >
+        <button 
+          onClick={() => increaseBy(-1)} 
+          className={styles.buttonMinus}>
+            -
+        </button>
+        <div className={styles.countLabel}>{counter}</div>
+        <button 
+          onClick={() => increaseBy(+1)} 
+          className={`${styles.buttonAdd} ${isMaxReached() && styles.disabled}`}>
+            +
+        </button>
+      </div>
+  )
 }
--- src/02-component-patterns/interfaces/interfaces.ts
+++ src/02-component-patterns/interfaces/interfaces.ts
@@ -11,6 +11,7 @@ export interface Product {
 
 export interface ProductContextProps {
     counter:    number
+    maxCount?:  number 
     product:    Product
     increaseBy: (value: number) => void
 }
@@ -24,9 +25,23 @@ export interface ProductCardHOCProps {
 
 export interface onChangeArgs {
     product: Product
-    count: number
+    count:   number
 }
 
 export interface ProductInCart extends Product {
-  count: number
+    count: number
+}
+
+export interface InitialValues {
+    count?:    number
+    maxCount?: number
+}
+
+export interface ProductCardHandlers {
+    count:             number
+    isMaxCountReached: boolean
+    maxCount?:         number
+    product:           Product
+    increaseBy:        (value: number) => void
+    reset:             () => void
 }
